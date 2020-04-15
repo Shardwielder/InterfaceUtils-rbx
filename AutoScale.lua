6f67d6779756e2afc2352dcc7a18ad4e9861e4cc
Andrew Holmes <holmesandrewv@gmail.com>
2020-04-15 18:30:16 +1200

    first commit

---
+++ AutoScale.lua
@@ -0,0 +1,80 @@
+--[[
+    A module which allows for more flexible, automatic scaling than what is 
+    provided using the Scale element of the GUI  instance size property.
+    Utilises UIScale objects, which also allows for texts to scale automatically, 
+    and much more precisely and nicely than using Scale sizing and TextScaled.
+
+    Usage:
+    - Design all interfaces in the same resolution, such as 1280x720. Only use
+    Offset for the sizing and positioning, do not use Scale. Define the standard
+    resolution that was used in `BASE_SCREEN_SIZE_X` and `BASE_SCREEN_SIZE_Y`.
+    The portion of the screen that UI elements take up at this resolution will
+    be the same portion they take up at any other resolution using this module.
+
+    - Add any UI elements (GUI instances) that you want to be scaled by calling 
+    `AutoScale.addElement` with the instance as the single parameter. Note
+    that this will also cause *all of its children* to be scaled as well.
+    Thus, it is best for performance to only use a single, root ScreenGui 
+    as the only element you add; this will additionally mean you only need to
+    call addElement once.
+
+    - You can alternatively create a UIScale object in the root ScreenGui
+    of your entire interface, and put it in the `scaleObjects` array.
+
+    Tips:
+    - Common resolutions are best used for the base resolution, such as 1920x1080.
+    A 16:9 aspect ratio is advised.
+    - The resolution can be forced in Studio by going to Test -> Device, selecting
+    your chosen resolution, and then selecting "Fit to Window" in the header dropdown.
+    - Anchor points must be utilised for any GUI elements which need to "hang" to any
+    side of the screen; otherwise, the positioning will be inaccurate.
+    - The `SNAP_RESOLUTION_STEP` variable defines the percentage increment at which
+    the `BASE_SCREEN_SIZE` is scaled. This should be kept so that 
+    BASE_SCREEN_SIZE * SNAP_RESOLUTION_STEP(n) always equals an integer, otherwise 
+    images will lose their clarity.
+]]
+
+local RunService = game:GetService("RunService")
+
+local BASE_SCREEN_SIZE_X = 1280
+local BASE_SCREEN_SIZE_Y = 720
+
+local SNAP_RESOLUTION_STEP = 0.05
+local NON_INSTANCE_ERROR = "Bad argument %s to %s: must be an Instance"
+
+local cameraObject = workspace.CurrentCamera
+local scaleObjects = {}
+
+local AutoScale = {}
+
+--[[
+    Add any GUI instance (and thus, all of its children) to be automatically scaled.
+    It is suggested that this is done with a single base ScreenGui.
+]]
+function AutoScale.addElement(guiObject)
+    assert(typeof(guiObject) == "Instance", NON_INSTANCE_ERROR:format("#1 guiObject", "Scaling:addElement"))
+    local scaleObject = Instance.new("UIScale")
+    scaleObjects.Name = "AutoScreenScale"
+    scaleObject.Scale = 1
+    scaleObject.Parent = guiObject
+    scaleObjects[#scaleObjects + 1] = scaleObject
+end
+
+-- Update all bound UIScale objects based on Y axis resolution scale
+local function updateScaleObjects()
+    local portionY = cameraObject.ViewportSize.Y / BASE_SCREEN_SIZE_Y
+    local scaleY = math.floor(portionY / SNAP_RESOLUTION_STEP) * SNAP_RESOLUTION_STEP
+
+    for i, scaleObject in ipairs(scaleObjects) do
+        scaleObject.Scale = scaleY
+    end
+end
+RunService.Heartbeat:connect(updateScaleObjects)
+
+workspace:GetPropertyChangedSignal("CurrentCamera"):Connect(function()
+    if workspace.CurrentCamera then
+        cameraObject = workspace.CurrentCamera
+    end
+end)
+
+return AutoScale
---
+++ RoundCorners.lua
@@ -0,0 +1,83 @@
+--[[
+    Call `roundElement` (guiObject: instance, cornerRadius: number) to convert
+    any Frame, ImageLabel, or ImageButton into one with a rounded background of
+    `cornerRadius`.
+
+    If the instance is a Frame, it will be replaced with an ImageLabel
+    of the same transferrable properties. The BackgroundColor3 and BackgroundTransparency
+    properties of the Frame will be used as the basis for the ImageColor3 and
+    ImageTransparency properties of the generated ImageLabel.
+]]
+
+local NON_NUMERICAL_ERROR = "Bad argument %s to %s: must be a number"
+local NON_COMPATIBLE_GUI_ERROR = "Bad argument %s to %s: must be a ImageLabel, ImageButton, or Frame"
+local IMPOSSIBLE_RADIUS_ERROR = "Bad argument %s to %s: corner radius must be greater than 0"
+local NO_RADIUS_ERROR = "Bad argument %s to %s: no image present for a corner radius of %s"
+
+--[[
+    The key for each entry corresponds to the corner radius of the
+    image asset (value). The  foreground of the image must be completely white,
+    the background completely transparent, and the image fully circular
+    (with a diameter of key cornerRadius) or else unexpected results will appear.
+]]
+local CIRCULAR_IMAGES = {
+    [2] = "rbxassetid://4900818720",
+    [3] = "rbxassetid://4900818812",
+    [4] = "rbxassetid://4900818868",
+    [5] = "rbxassetid://4900818934",
+    [6] = "rbxassetid://4900818981",
+    [7] = "rbxassetid://4900818981",
+    [8] = "rbxassetid://4900819077",
+    [9] = "rbxassetid://4900819126",
+    [10] = "rbxassetid://4900819170",
+}
+
+local transitionableProperties = {
+    "ZIndex", "ClipsDescendants", "Visible", "AutoLocalize", "RootLocalizationTable", "LayoutOrder",
+    "Name", "Size", "Position", "AnchorPoint", "Rotation", "Active", "Selectable",
+}
+
+local RoundCorners = {}
+
+local function prepareGuiObject(imageAssetId, guiObject, cornerRadius)
+    if guiObject.ClassName == "Frame" then
+        local newGuiObject = Instance.new("ImageLabel")
+        for i, propertyName in ipairs(transitionableProperties) do
+            newGuiObject[propertyName] = guiObject[propertyName]
+        end
+        newGuiObject.ImageColor3 = guiObject.BackgroundColor3
+        newGuiObject.ImageTransparency = guiObject.BackgroundTransparency
+        for i, instance in pairs(guiObject:GetChildren()) do
+            instance.Parent = newGuiObject
+        end
+        newGuiObject.Parent = guiObject.Parent
+        guiObject:Destroy()
+        guiObject = newGuiObject
+    elseif guiObject.ClassName ~= "ImageButton" and guiObject.ClassName ~= "ImageLabel" then
+        return
+    end
+
+    guiObject.BackgroundTransparency = 1
+    guiObject.BorderSizePixel = 0
+    guiObject.Image = imageAssetId
+    guiObject.ScaleType = Enum.ScaleType.Slice
+    guiObject.SliceCenter = Rect.new(cornerRadius, cornerRadius, cornerRadius, cornerRadius)
+    guiObject.SliceScale = 1
+    return guiObject
+end
+
+function RoundCorners.roundElement(guiObject, cornerRadius)
+    assert(guiObject, NON_COMPATIBLE_GUI_ERROR:format("#1 guiObject", "RoundCorners.roundElement"))
+    assert(type(cornerRadius) == "number", NON_NUMERICAL_ERROR:format("#2 cornerRadius", "RoundCorners.roundElement"))
+    assert(cornerRadius > 0, IMPOSSIBLE_RADIUS_ERROR:format("#2 cornerRadius", "RoundCorners.roundElement"))
+
+    local imageAssetId = CIRCULAR_IMAGES[cornerRadius]
+    assert(imageAssetId, NO_RADIUS_ERROR:format("#2 cornerRadius", "RoundCorners.roundElement", cornerRadius))
+
+    guiObject = prepareGuiObject(imageAssetId, guiObject, cornerRadius)
+    assert(guiObject, NON_COMPATIBLE_GUI_ERROR:format("#1 guiObject", "RoundCorners.roundElement"))
+
+    return guiObject
+end
+
+return RoundCorners
